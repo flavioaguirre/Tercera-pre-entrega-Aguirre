af221ea3633f9f5141a3ec3f823c75dd8ed728d2
Flavio Aguirre <flavioaguirre0@gmail.com>
2023-10-30 21:57:48 -0300

    fin app empleos 2

--- empleos/urls.py
+++ empleos/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('lista_empleos/', views.lista_empleos, name='empleos'),
-    path('publicar_empleos/', views.publicar_empleo, name='pubicar_empleos'),
     path('lista_empleos/<int:id>', views.detalle_empleo, name='detalle_empleo'),
+    path('publicar_empleos/', views.publicar_empleo, name='pubicar_empleos'),
 ]
